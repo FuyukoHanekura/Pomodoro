3db930d6fea0b7b2be122283947e055c23aedb98
BEVIS-SURPRISE_Thomas <bevissurprise.thomas@gmail.com>
2025-06-09 23:19:41 +0200

    feat: pomodoro prototype

--- Pomodoro/MainWindow.xaml.cs
+++ Pomodoro/MainWindow.xaml.cs
@@ -1,13 +1,7 @@
-﻿using System.Text;
+﻿using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
+
 
 namespace Pomodoro
 {
@@ -16,9 +10,74 @@ namespace Pomodoro
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		private const double TIMER_START = 25;
+		private DispatcherTimer timer;
+		private TimeSpan timeLeft;
+		private bool isRunning;
+
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			timeLeft = TimeSpan.FromMinutes(TIMER_START);
+
+			timer = new DispatcherTimer();
+			timer.Interval = TimeSpan.FromSeconds(1);
+			timer.Tick += Timer_Tick;
+
+			UpdateTimerDisplay();
 		}
+
+		private void UpdateTimerDisplay()
+		{
+			TimerLabel.Content = timeLeft.ToString(@"mm\:ss");
+		}
+
+		private void Timer_Tick(object? sender, EventArgs e)
+		{
+			if (timeLeft.TotalSeconds > 0)
+			{
+				timeLeft = timeLeft.Subtract(TimeSpan.FromSeconds(1));
+				UpdateTimerDisplay();
+			}
+			else
+			{
+				timer.Stop();
+				isRunning = false;
+				MessageBox.Show("Pomodoro Complete!");
+			}
+		}
+
+		private void OnStartBtn(object? sender, EventArgs e)
+		{
+			if (!isRunning)
+			{
+				timer.Start();
+				isRunning = true;
+			}
+		}
+
+		private void OnPauseBtn(object? sender, EventArgs e)
+		{
+			if (isRunning)
+			{
+				timer.Stop();
+				isRunning = false;
+				StartBtn.Content = "Resume";
+			}
+		}
+
+		private void OnResetBtn(object? sender, EventArgs e)
+		{
+			if (!isRunning)
+			{
+				timer.Stop();
+				isRunning = false;
+				timeLeft = TimeSpan.FromMinutes(TIMER_START);
+				UpdateTimerDisplay();
+				StartBtn.Content = "Start";
+			}
+		}
+
 	}
 }
